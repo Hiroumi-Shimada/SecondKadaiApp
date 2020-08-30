f049f63816d13a30a9568625613fb9ea68b438c7
Hiroumi Shimada <hiroumi0713@gmail.com>
2020-08-30 13:39:34 +0900

    Initial Commit

---
+++ SecondKadaiApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SecondKadaiApp
+//
+//  Created by Hiroumi Shimada on 2020/08/30.
+//  Copyright © 2020 Hiroumi Shimada. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
